751eb0883e29bd188092c53be1a929d3d22e0942
Zhenua <zhenua.gladkovv@gmail.com>
2021-04-07 00:38:55 +0300

    pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 
-const { src, dest, watch, parallel, series } = require ('gulp');
+const { src, dest, watch, parallel, series, task } = require ('gulp');
 
+const gulp        = require('gulp');
 const scss         = require('gulp-sass');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
@@ -8,6 +9,50 @@ const uglify       = require('gulp-uglify-es').default;
 const autoprefixer = require ('gulp-autoprefixer');
 const imagemin     = require('gulp-imagemin');
 const del          = require('del');
+
+const pug = require('gulp-pug');
+
+// function add() {
+//     task('pug', function buildHTML() {
+//         return src('app/Pug/*.pug')
+//         .pipe(pug({
+//             pretty:true
+//         }))
+//         .pipe(dest('app'));
+//       });
+// }
+
+//   task('pug', function buildHTML() {
+//   return src('app/Pug/*.pug')
+//   .pipe(pug({
+//       pretty:true
+//   }))
+//   .pipe(dest('app'));
+// });
+const paths = {
+    src: 'src',
+    dist: 'dist'
+  };
+
+const pugFiles = {
+    src: paths.src + 'app/Pug/*.pug',
+    dist: paths.dist,
+    watch: paths.src + 'app/Pug/*.pug'
+  }
+  
+  gulp.task('pug', function() {
+    gulp.src(pugFiles.src)
+      .pipe(pug({
+        locals: {},
+        pretty: true
+      }))
+      .pipe(gulp.dest(pugFiles.dist))
+  });
+  function reloadBrowserSyncPug() {
+    setTimeout(reloadBrowserSync, 500);
+  }
+  
+
 
 
 function browsersync() {
@@ -71,15 +116,29 @@ function build () {
         'app/css/style.min.css',
         'app/fonts/**/*', // ** - это все папки, /* - это все файлы
         'app/js/main.min.js',
-        'app/*.html'
+        'app/*.html',
+        'app/Pug/*pug'
     ], {base:'app'})
     .pipe(dest('dist'))
 }
+
+
 
 function watching(){
     watch(['app/scss/**/*.scss'], styles );
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts ); // ! - этот знак означает кроме меня
     watch(['app/*.html']).on('change', browserSync.reload);
+    // task('watch', function () {
+    //     return watch('app/Pug/*.pug', { ignoreInitial: false })
+    //     .pipe(dest('app'));
+    //     });
+
+
+        //  watch(['app/Pug/*.pug'], add );
+    task('watch', [], function() {
+        watch(pugFiles.watch, ['pug', reloadBrowserSyncPug]);
+    })
+
 }
 
 exports.styles = styles;
@@ -88,6 +147,10 @@ exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.images = images;
 exports.cleanDist = cleanDist;
+
+
+
+
 
 exports.build = series(cleanDist, images, build );
 exports.default = parallel(styles ,browsersync, watching, scripts);
